fc5d78da376e0fbab85b84ef20e3ebb01f8e856d
ugniusalekna <ugnius.alekna@gmail.com>
2024-09-11 22:15:10 +0300

    initial commit

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ app4 = create_app4()
 app4.init_app(server)
 
 app6 = create_app6()
-app6.init_app(server)
 server.register_blueprint(app6, url_prefix='/tensorboard-view')
 
 if __name__ == "__main__":
