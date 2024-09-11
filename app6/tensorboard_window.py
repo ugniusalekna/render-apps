016dab338ea55966598c7f0f356ad217ac98a070
ugniusalekna <ugnius.alekna@gmail.com>
2024-09-11 22:30:48 +0300

    initial commit

--- app6/tensorboard_window.py
+++ app6/tensorboard_window.py
@@ -9,11 +9,12 @@ def create_app():
 
     @bp.route('/tensorboard')
     def tensorboard():
-        subprocess.Popen(['tensorboard', '--logdir', TENSORBOARD_LOGDIR, '--host', '0.0.0.0', '--port', '6006'])
+        port = 6006
+        subprocess.Popen(['tensorboard', '--logdir', TENSORBOARD_LOGDIR, '--host', '0.0.0.0', '--port', str(port)])
 
-        tensorboard_template = '''
+        tensorboard_template = f'''
         <h1>TensorBoard Dashboard</h1>
-        <iframe src="http://localhost:6006" width="100%" height="800px"></iframe>
+        <iframe src="http://0.0.0.0:{port}" width="100%" height="800px"></iframe>
         '''
         return render_template_string(tensorboard_template)
 
